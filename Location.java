51094b4195eab50a8fbf15c6cf3f6032373c54a2
otboss <otsurfer6@gmail.com>
2019-10-07 20:59:42 +0800

    initial commit

---
+++ Location.java
@@ -0,0 +1,10 @@
+
+class Location {
+    public double xcoord;
+    public double ycoord;
+
+    Location(double xcoord, double ycoord) {
+        this.xcoord = xcoord;
+        this.ycoord = ycoord;
+    }
+}
